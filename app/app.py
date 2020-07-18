279508656688708883eb76940657bee258c79a2e
Kevin Azoulay <azoulay.kevin@gmail.com>
2020-07-18 03:56:55 +0200

    dbtest yields json users db

--- app/app.py
+++ app/app.py
@@ -1,9 +1,21 @@
 #! /usr/bin/env python3
 
+import os
+import mariadb
+import json
 from flask import Flask
 
 
 app = Flask(__name__)
+
+# configuration used to connect to MariaDB
+config = {
+    'host': 'db',
+    'port': 3306,
+    'user': os.environ['MYSQL_USER'],
+    'password': os.environ['MYSQL_PASSWORD'],
+    'database': os.environ['MYSQL_DATABASE'],
+}
 
 
 @app.route("/")
@@ -13,6 +25,27 @@ def Coucou():
 @app.route("/route")
 def CoucouRoute():
     return "Encore moi"
+
+@app.route("/users/<user>")
+def user_page(user):
+    return "Coucou " + user
+
+@app.route("/dbtest")
+def dbtest():
+    # connection for MariaDB
+    conn = mariadb.connect(**config)
+    # create a connection cursor
+    cur = conn.cursor()
+    # execute a SQL statement
+    cur.execute("select * from users")
+    header = [item[0] for item in cur.description]
+    res = cur.fetchall()
+    json_data=[]
+    for item in res:
+        json_data.append(dict(zip(header,item)))
+
+    # return the results!
+    return json.dumps(json_data)
 
 
 if __name__ == "__main__":
