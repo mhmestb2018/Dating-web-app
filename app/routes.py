c2bd6c9c68b98465c932df611b72b5cd20429879
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2020-07-27 20:47:40 +0200

    moving on with templates

--- app/routes.py
+++ app/routes.py
@@ -2,30 +2,41 @@
 
 import json
 from flask import current_app as app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, session
 
 # configuration used to connect to MariaDB
 
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
 @app.route("/login/", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        pass
-    else:
-        return render_template("login.html")
+        user = request.form["nm"]
+        if user is not None and user is not "":
+            session["user"] = user
+            return redirect(url_for("profile"))
+    return render_template("login.html")
 
 
 @app.route("/uri/")
 def CoucouRoute():
     return app.config['SQLALCHEMY_DATABASE_URI']
 
+def profile_page(user):
+    return render_template("base.html", title=f"{user}", body=f"<p>Bonjour {user}</p>")
+
 @app.route("/users/<user>/")
 def users(user):
-    return render_template("base.html", title=f"{user}", body=f"<p>Bonjour {user}</p>")
+    return profile_page(user)
+
+@app.route("/profile/")
+def profile():
+    if "user" in session:
+        return profile_page(session["user"])
+    return redirect(url_for("index"))
 
 
 @app.route('/testadd/', methods=['GET'])
