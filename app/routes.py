726928f6f6dae8a64d3f9c3be47412936dbc5ed1
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2020-07-28 11:43:55 +0200

    login broken as User not serializable

--- app/routes.py
+++ app/routes.py
@@ -10,37 +10,52 @@ from .test_routes import *
 def index():
     return render_template("index.html")
 
+@app.route("/signin/", methods=["POST", "GET"])
+def signin_page():
+    if "user" in session:
+        flash(f"Vous êtes déjà connecté !", "info")
+        return redirect(url_for("profile"))
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        # password = request.form["password"]
+        if User.query.filter_by(email=email).first():
+            flash("Vous avez déjà un compte")
+            return redirect(url_for("login"))
+        user = User(name=name, email=email)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Compte créé, merci de vous connecter")
+        return redirect(url_for("login"))
+
+    return render_template("signin.html")
+
 @app.route("/login/", methods=["POST", "GET"])
 def login():
-    if request.method == "POST":
-        name = request.form["nm"]
-        if name is not None and name is not "":
-            session["user"] = name
-            #if cookies accepted:
-            session.permanent = True
-
-            found = User.query.filter_by(name=name).first()
-            if found:
-                session["email"] = found.email
-            else:
-                user = User(name, "")
-                db.session.add(user)
-                db.session.commit()
-
-
-            flash(f"Welcome {name}")
-            return redirect(url_for("profile"))
     if "user" in session:
         flash("Vous êtes déjà connecté !", "info")
+        return redirect(url_for("profile"))
+    if request.method == "POST":
+        email = request.form["email"]
+        found = User.query.filter_by(email=email).first()
+        if found:
+            session["user"] = found
+            #if GDPR accepted ?:
+            session.permanent = True
+        else:
+            flash("Ce compte n'existe pas")
+            return render_template("login.html")
+        # Left to check password
+
+        flash(f"Bienvenue {found.name}")
         return redirect(url_for("profile"))
     return render_template("login.html")
 
 @app.route("/logout/")
 def logout():
     if "user" in session:
-        flash(f"{session['user']} déconnecté", "info")
+        flash(f"{session['user'].name} déconnecté", "info")
         session.pop("user", None)
-        session.pop("email", None)
     return redirect(url_for("login"))
 
 @app.route("/users/<user>/")
@@ -50,17 +65,13 @@ def users(user):
 @app.route("/profile/", methods=["POST", "GET"])
 def profile():
     if "user" in session:
-        email = None
+        user = session["user"]
         if request.method == "POST":
             found = User.query.filter_by(name=session["user"]).first()
-            if "email" in request.form and request.form["email"] is not "":
-                found.email = request.form["email"]
-                db.session.commit()
-                email = found.email
+            if "email" in request.form:
+                user.email = request.form["email"]
                 flash("Email enregistré", "info")
-        else:
-            if "email" in session:
-                email = session["email"]
-        return render_template("profile_edit.html", user=session["user"], email=email)
+            db.session.commit()
+        return render_template("profile_edit.html", user=session["user"])
     flash("Vous n'êtes pas connecté", "info")
     return redirect(url_for("login"))
