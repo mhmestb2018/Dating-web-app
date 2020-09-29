e6a42251c48a20227c61900f42f5c66210e8fbc5
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2020-09-30 00:01:19 +0200

    ping loop added to test to wait for everything to start on slow configurations (hi Github CI)

--- back/tests/main.py
+++ back/tests/main.py
@@ -4,6 +4,18 @@ from utils import (signup, login, validate, create, update, delete,
                     logout, get_profile, get_public_profile, like, unlike, block, unblock,
                     report)
 
+def wait_startup():
+    response = None
+    i = 0
+    while response is None and i < 30:
+        try:
+            response = user1["session"].get({"url"})
+        except:
+            print("Not able to reach backend yet, waiting 3 seconds...", flush=True)
+            time.sleep(3)
+        i += 1
+    assert response.status_code == 200
+
 def test_clean_failed_attempts():
     login(user1)
     delete(user1)
