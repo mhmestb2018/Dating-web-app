e341745b6cb93bc0a60cd9798f1f6341ec54adf9
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2021-04-12 01:34:16 +0200

    now emiiting a notif at socket join

--- back/dating_api/routes/sockets.py
+++ back/dating_api/routes/sockets.py
@@ -1,9 +1,8 @@
 from .. import socketio
 
-from flask_socketio import join_room, leave_room
-
+from flask_socketio import join_room, emit
 @socketio.on('join')
 def on_join(data):
     room = data['room']
     join_room(room)
-    
+    emit('notification', {'Info': "Vous êtes connecté."}, room=room)
